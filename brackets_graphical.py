ac4bce31e4208008fd9db79e0417e3e077b8e2b6
thomas.chaverondier@gmail.com <thomas.chaverondier@gmail.com>
2015-11-06 09:44:01 +0100

    optimized the scrollbars' behaviour (adapt to canvas size and react to mousewheel)

--- brackets_graphical.py
+++ brackets_graphical.py
@@ -9,35 +9,62 @@ class Window :
 
 		self.master = Tk()
 		self.master.title("Brackets... brackets everywhere !")
-		#self.master.geometry("1300x650")          # seems kinda pointless
+		#self.master.geometry("1300x650")          # just in case of
 
-		self.see = True
+		# -__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__- #
+		# -__-__-__-__-__-__-__-__-__-                                      Window's Layout                                       -__-__-__-__-__-__-__-__-__- #
+		# -__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__- #
 
-		# both probably rendered useless, as the Canvas is now virtually infinite
-		#self.w = 1300
-		#self.h = 1000
+		# dimensions of the main window, according to the user's device
+		self.w = self.master.winfo_screenwidth() * 0.8
+		self.h = self.master.winfo_screenheight() * 0.8
 
+		# master frame
+		frame = Frame(self.master)
+		frame.pack()
+
+		# scrollbars
+		scrollbarv = Scrollbar(frame)
+		scrollbarv.pack(side=RIGHT,fill=Y)
+		scrollbarh = Scrollbar(frame,orient=HORIZONTAL)
+		scrollbarh.pack(side=BOTTOM,fill=X)
+
+		# canvas in which we draw the brackets
+		self.tree = Canvas(frame,bg='white',xscrollcommand=scrollbarh.set,yscrollcommand=scrollbarv.set,width=self.w,height=self.h)
+		self.tree.bind("<Button-4>", self.on_mousewheel)
+		self.tree.bind("<Button-5>", self.on_mousewheel)
+		self.tree.bind("<Configure>",self.on_configure)
+		self.tree.pack()
+
+		scrollbarh.config(command=self.tree.xview)
+		scrollbarv.config(command=self.tree.yview)
+
+		# -__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__- #
+		# -__-__-__-__-__-__-__-__-__-                                                                                            -__-__-__-__-__-__-__-__-__- #
+		# -__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__- #
+
+
+		# size of elements in brackets
 		self.x_size = 80
 		self.y_size = 40
 		self.x_gap = 20
 		self.y_gap = 8
 
-		frame = Frame(self.master)
-		frame.pack()
 
-		scrollbar = Scrollbar(frame)
-		scrollbar.pack(side=RIGHT,fill=Y)
+	# vertical slider to be active on mousewheel
+	def on_mousewheel(self, event):
+		direction = 1
+		if event.num == 4 :
+			direction = -1
+		self.tree.yview_scroll(direction, "units")
 
-		self.tree = Canvas(frame,bg='white',scrollregion=(0,0,1000,1000),yscrollcommand=scrollbar.set)
-		self.tree.bind_all("<MouseWheel>", self._on_mousewheel)
-		self.tree.pack()
-
-		scrollbar.config(command=self.tree.yview)
-
-	def _on_mousewheel(self, event):
-		self.tree.yview_scroll(event.delta, "units")
+	# adapting the range of the vertical slider
+	def on_configure(self, event) :
+		self.tree.configure(scrollregion=self.tree.bbox('all'))
 
 
+
+	#         |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||         #
 
 	def draw_sb(self, nb_teams = 16) :
 
@@ -66,5 +93,5 @@ class Window :
 
 
 pouet = Window()
-pouet.draw_sb(32)
+pouet.draw_sb(256)
 pouet.master.mainloop()
